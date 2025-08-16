7c12e21d442e054a5653c490f15228f6a65f7b0a
Dave <mckinnondavidj@gmail.com>
2025-08-15 20:42:12 -0700

    comments

--- PicoGPT/PicoGPT.c
+++ PicoGPT/PicoGPT.c
@@ -27,6 +27,38 @@ int main()
     printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
     // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
 
+    // 32 ms per attention head
+    // That's Softmax(QKT)/rootd*V
+    // with residual skip
+    // and fully connected
+    // actually 2 fullys
+    // no layernorm yet
+    // but some of this is in floateger
+
+    /* whole model is
+     * Layernorm
+     * Attention     |
+     * Residual      |
+     * MLP           |
+     * Residual      |
+     * Layernorm
+     * Attention
+     * Layernorm
+     * MLP
+     * Layernorm
+     * Fully Connected to convert to token space
+     * Softmax
+     * Probabilistic sampling thingie
+     * 
+     * The highlighted part takes ~32ms
+     * Double that, since we are doing two heads. Add a fudge factor since more layernorm
+     * Even with float stuff, this is not too slow
+     * That's maybe 80ms per token, call it 100ms for the extra stuff
+     * So 10 tok/s
+     * That is so easily fast enough. That's even without any fancy nonsense
+     * Of course, question of accuracy, and I do need to perform _some_ quantisation
+     * But this is very feasible I think
+     */
     attention_test();
 
     
